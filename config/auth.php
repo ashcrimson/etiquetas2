ab3ef930b80144896901086d487bc73a77b3f38f
Felipe Pino <felipepinoredes@gmail.com>
2021-03-26 14:19:25 -0300

    paquete oracle funcional

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
+            'driver' => 'oracle',
             'model' => App\Models\User::class,
         ],
 
