ca132cc70a23a2dab5009f1cb132086b99ee144c
unknown <felipepinoredes@gmail.com>
2021-03-31 10:12:41 -0300

    botones

--- app/DataTables/ConvenioDataTable.php
+++ app/DataTables/ConvenioDataTable.php
@@ -116,9 +116,7 @@ class ConvenioDataTable extends DataTable
                 'responsive' => true,
                 'buttons' => [
                     ['extend' => 'create', 'text' => '<i class="fa fa-plus"></i> <span class="d-none d-sm-inline">Crear</span>'],
-                    ['extend' => 'print', 'text' => '<i class="fa fa-print"></i> <span class="d-none d-sm-inline">Imprimir</span>'],
                     ['extend' => 'reload', 'text' => '<i class="fa fa-sync-alt"></i> <span class="d-none d-sm-inline">Recargar</span>'],
-                    ['extend' => 'reset', 'text' => '<i class="fa fa-undo"></i> <span class="d-none d-sm-inline">Reiniciar</span>'],
                     ['extend' => 'export', 'text' => '<i class="fa fa-download"></i> <span class="d-none d-sm-inline">Exportar</span>'],
                 ],
             ]);
