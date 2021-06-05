2f94ba4a049f49b500ff4e48f506803431ff4268
Felipe Pino <felipepinoredes@gmail.com>
2021-06-04 21:38:44 -0400

    Prueba Login Ldap

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -61,40 +61,22 @@ class LoginController extends Controller
 
         $this->validateLogin($request);
 
+        $usuario = (strstr($request->email, '@', true) . "\n");
+        $dominio = (str_replace("@", "", strstr($usuario, '@')) . "\n");
+
         $params = array(
-            "id" => $request->username,
+            "id" => $usuario,
             "clave" => $request->password,
         );
-
 
         $client = new nusoap_client('http://172.25.16.18/bus/webservice/ws.php?wsdl');
         $response = $client->call('autentifica_ldap', $params);
 
 
-        return $response;
+        dd($response);
 
 
 
-        // If the class is using the ThrottlesLogins trait, we can automatically throttle
-        // the login attempts for this application. We'll key this by the username and
-        // the IP address of the client making these requests into this application.
-        if (method_exists($this, 'hasTooManyLoginAttempts') &&
-            $this->hasTooManyLoginAttempts($request)) {
-            $this->fireLockoutEvent($request);
-
-            return $this->sendLockoutResponse($request);
-        }
-
-        if ($this->attemptLogin($request)) {
-            return $this->sendLoginResponse($request);
-        }
-
-        // If the login attempt was unsuccessful we will increment the number of attempts
-        // to login and redirect the user back to the login form. Of course, when this
-        // user surpasses their maximum number of attempts they will get locked out.
-        $this->incrementLoginAttempts($request);
-
-        return $this->sendFailedLoginResponse($request);
 
     }
 
