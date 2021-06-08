52a6558d0b65226b97a623f5f0508bf0d2ff2e3c
Felipe Pino <felipepinoredes@gmail.com>
2021-06-08 11:12:05 -0400

    otra cosa chica

--- resources/views/pacientes/datatables_actions.blade.php
+++ resources/views/pacientes/datatables_actions.blade.php
@@ -2,13 +2,13 @@
     <i class="fa fa-eye"></i>
 </a>
 
-<a href="{{ route('pacientes.edit', $id) }}" data-toggle="tooltip" title="Editar" class='btn btn-outline-info btn-sm'>
+<!-- <a href="{{ route('pacientes.edit', $id) }}" data-toggle="tooltip" title="Editar" class='btn btn-outline-info btn-sm'>
     <i class="fa fa-edit"></i>
 </a>
 
 <a href="#" onclick="deleteItemDt(this)" data-id="{{$id}}" data-toggle="tooltip" title="Eliminar" class='btn btn-outline-danger btn-sm'>
     <i class="fa fa-trash-alt"></i>
-</a>
+</a> -->
 
 
 <form action="{{ route('pacientes.destroy', $id)}}" method="POST" id="delete-form{{$id}}">
