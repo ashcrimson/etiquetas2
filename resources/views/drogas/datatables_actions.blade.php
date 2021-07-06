800c98a682547600eb6605c792911c7d629f51ed
Edras Altamirano <altamiranoesdras@gmail.com>
2021-07-05 19:09:24 -0600

    Comentados permisos datatables_actions.blade.php drogas

--- resources/views/drogas/datatables_actions.blade.php
+++ resources/views/drogas/datatables_actions.blade.php
@@ -1,23 +1,23 @@
-@can('Ver drogas')
+{{--@can('Ver drogas')--}}
 <a href="{{ route('drogas.show', $id) }}" data-toggle="tooltip" title="Ver" class='btn btn-default btn-sm'>
     <i class="fa fa-eye"></i>
 </a>
-@endcan
+{{--@endcan--}}
 
-@can('Editar drogas')
+{{--@can('Editar drogas')--}}
 <a href="{{ route('drogas.edit', $id) }}" data-toggle="tooltip" title="Editar" class='btn btn-outline-info btn-sm'>
     <i class="fa fa-edit"></i>
 </a>
-@endcan
+{{--@endcan--}}
 
 
-@can('Editar drogas')
+{{--@can('Imprimir drogas')--}}
     <a href="{{ route('drogas.imprimir', $id) }}" data-toggle="tooltip" title="Imprimir" class='btn btn-outline-secondary btn-sm'>
         <i class="fa fa-print"></i>
     </a>
-@endcan
+{{--@endcan--}}
 
-@can('Eliminar Drogras')
+{{--@can('Eliminar Drogras')--}}
 <a href="#" onclick="deleteItemDt(this)" data-id="{{$id}}" data-toggle="tooltip" title="Eliminar" class='btn btn-outline-danger btn-sm'>
     <i class="fa fa-trash-alt"></i>
 </a>
@@ -27,4 +27,4 @@
     @method('DELETE')
     @csrf
 </form>
-@endcan
+{{--@endcan--}}
