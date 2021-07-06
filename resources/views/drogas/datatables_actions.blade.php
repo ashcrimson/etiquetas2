b58db5b39be90b4d82932b7c6a8c5970562095e4
Edras Altamirano <altamiranoesdras@gmail.com>
2021-07-05 19:00:02 -0600

    Boton para imprimir show droga

--- resources/views/drogas/datatables_actions.blade.php
+++ resources/views/drogas/datatables_actions.blade.php
@@ -1,16 +1,23 @@
-@can('drogas.show')
+@can('Ver drogas')
 <a href="{{ route('drogas.show', $id) }}" data-toggle="tooltip" title="Ver" class='btn btn-default btn-sm'>
     <i class="fa fa-eye"></i>
 </a>
 @endcan
 
-@can('drogas.edit')
+@can('Editar drogas')
 <a href="{{ route('drogas.edit', $id) }}" data-toggle="tooltip" title="Editar" class='btn btn-outline-info btn-sm'>
     <i class="fa fa-edit"></i>
 </a>
 @endcan
 
-@can('drogas.destroy')
+
+@can('Editar drogas')
+    <a href="{{ route('drogas.imprimir', $id) }}" data-toggle="tooltip" title="Imprimir" class='btn btn-outline-secondary btn-sm'>
+        <i class="fa fa-print"></i>
+    </a>
+@endcan
+
+@can('Eliminar Drogras')
 <a href="#" onclick="deleteItemDt(this)" data-id="{{$id}}" data-toggle="tooltip" title="Eliminar" class='btn btn-outline-danger btn-sm'>
     <i class="fa fa-trash-alt"></i>
 </a>
